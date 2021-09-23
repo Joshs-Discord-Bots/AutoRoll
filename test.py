db91c729f3d1ce20ebab2222f705fc35f2f47829
Joshuahuahua <joshuahollander2001@gmail.com>
2021-09-23 17:05:14 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from deep_translator import GoogleTranslator
-
-
-
-to_translate = '有趣的休息室'
-translated = GoogleTranslator(source='auto', target='en').translate(to_translate)
-# translated = GoogleTranslator(source='auto', target='de').translate(to_translate)
-print(translated)
