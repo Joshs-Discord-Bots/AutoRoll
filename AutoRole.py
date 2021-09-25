10eee84aead86b7892f8bb11a6396546a7787c0c
Joshuahuahua <joshuahollander2001@gmail.com>
2021-09-25 23:02:30 +0800

    Update AutoRole.py

--- AutoRole.py
+++ AutoRole.py
@@ -113,7 +113,7 @@ async def botreact(ctx, type, channelID, messageID, reaction):					# react comma
 
 @bot.command()
 async def ping(ctx):
-	await ctx.send('Test')
+	await ctx.send('Pong!')
 
 @bot.command()
 async def reload(ctx):
