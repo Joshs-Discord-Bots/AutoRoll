eccdb2ac8532532aec3f3f08bdfd02c0b37de831
Joshuahuahua <joshuahollander2001@gmail.com>
2021-09-26 00:29:54 +0800

    Update AutoRole.py

--- AutoRole.py
+++ AutoRole.py
@@ -17,6 +17,7 @@ activity = discord.Game(name=f"{prefix}help")
 bot = commands.Bot(command_prefix = prefix, intents=intents, activity=activity, status=discord.Status.online, case_insensitive=True)
 bot.remove_command('help')
 
+bot.id = bot.user.id
 
 bot.token = 'TOKEN'
 with open('token.txt') as f:
