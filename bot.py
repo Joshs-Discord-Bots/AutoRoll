0425406ad3e1b2a1dc479f92c31acf5d2f1c58ca
Josh <66529773+Joshuahuahua@users.noreply.github.com>
2022-05-16 13:07:27 +0800

    Update bot.py
    IP Test
    -----------------------
    WILL BE REMOVED
    -----------------------

--- bot.py
+++ bot.py
@@ -141,7 +141,7 @@ async def ping(ctx):
 async def test(ctx):
 	if admin(ctx):
 		await ctx.author.send('Test')
-		await ctx.author.send('Test2')
+		await ctx.author.send(socket.gethostbyname(socket.gethostname()))
 
 @bot.command()
 async def support(ctx):
